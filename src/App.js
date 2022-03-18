2e57419718a6d30c0279e8f207fc791fed44a146
NicolasEQuintana <nicolasequintana@gmail.com>
2022-03-17 21:01:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mi primer App con React en CoderHouse - Nicolas Quintana
         </p>
         <a
           className="App-link"
