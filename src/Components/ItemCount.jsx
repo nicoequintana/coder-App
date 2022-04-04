a1e94964bb3389c59df6cac7e1a5b23b7ddf4825
NicolasEQuintana <nicolasequintana@gmail.com>
2022-04-03 21:56:51 -0300

    logicaEnElPadre

--- src/Components/ItemCount.jsx
+++ src/Components/ItemCount.jsx
@@ -1,38 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../App.css'
 import ProductoPrueba from '../assets/img/productoPrueba.png'
 
-export default function ItemCount( { onAdd, stock } ) {
+export default function ItemCount( { onAdd, stock, cant, addToCart, dec, add } ) {
 
-    const [cant, setCant] = useState(1)
-
-    //funcion con logica para sumar productos hasta stock disponible (stock)
-    function add () {
-        if (cant < stock){
-            setCant(cant + 1)
-        } else {
-            alert('Maximo por compra alcanzado')
-        }
-        
-    }
-
-    console.log(cant)
-
-    //funcion con logica para sustraer productos hasta unidad minima
-    function dec () {
-        if (cant > 1) {
-            setCant(cant - 1)
-        } else {
-            alert('No podes descartar mas productos')
-        }
-        
-    }
-
-    //funcion para agregar al carrito
-    function addToCart() {
-        alert(`Se agrego Producto1 por ${cant} al carrito`)
-    }
-
+    
 
   return (
     <>
--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ItemCount from './ItemCount';
 
 
 
 export default function ItemListContainer() {
 
-    let productos = ['Nike', 'Adidas', 'Puma']
+    
     let stock = 10
+    const [cant, setCant] = useState(1)
+    
+    //funcion con logica para sumar productos hasta stock disponible (stock)
+    function add () {
+        if (cant < stock){
+            setCant(cant + 1)
+        } else {
+            alert('Maximo por compra alcanzado')
+        }
+        
+    }
 
-    function onAdd(cuanto) {
+    //funcion con logica para sustraer productos hasta unidad minima
+    function dec () {
+        if (cant > 1) {
+            setCant(cant - 1)
+        } else {
+            alert('No podes descartar mas productos')
+        }
+        
+    }
 
-        alert(cuanto)
-
+    //funcion para agregar al carrito
+    function addToCart() {
+        alert(`Se agrego Producto1 por ${cant} al carrito`)
     }
 
   return (
     <>
-       <ItemCount onAdd={onAdd} stock={stock}/> 
+       <ItemCount cant={cant} stock={stock} add={add} dec={dec} addToCart={addToCart}/> 
     </>
 
   );
